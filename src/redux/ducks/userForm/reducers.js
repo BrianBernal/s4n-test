3e5733eb4648633a10fadbb099b1e04783d5309c
BrianBernal <brianbernal27@gmai.com>
2020-12-20 01:44:28 -0500

    Setup form data cookie.

--- src/redux/ducks/userForm/reducers.js
+++ src/redux/ducks/userForm/reducers.js
@@ -1,10 +1,16 @@
 import * as types from './types';
 
-const initialState = {};
+// tomar de la cookie o dejar empty object {}
+const existCookie = document.cookie
+  .split('; ')
+  .find((row) => row.startsWith('userForm'));
+const initialState = existCookie ? JSON.parse(existCookie.split('=')[1]) : {};
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case types.USER_FORM_SAVE:
+      //  guardar la cookie
+      document.cookie = `userForm=${JSON.stringify(action.data)}; max-age=${60 * 60 * 24 * 30}`;
       return {
         ...state,
         ...action.data,
